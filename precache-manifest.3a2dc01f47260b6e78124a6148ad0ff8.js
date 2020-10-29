2bd88e9d8662781b6befacfd6cf9255383aa61de
= <=>
2020-10-29 14:02:40 +0100

    Updates

---
+++ precache-manifest.3a2dc01f47260b6e78124a6148ad0ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e0e7663828efb2432a26e913b82ae",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "83bde4cdd481b467cb07",
+    "url": "/react-calculator/static/css/main.e5dcec99.chunk.css"
+  },
+  {
+    "revision": "5e8d2ec1a3200a000961",
+    "url": "/react-calculator/static/js/2.c8763237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.c8763237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bde4cdd481b467cb07",
+    "url": "/react-calculator/static/js/main.ea826ba3.chunk.js"
+  },
+  {
+    "revision": "0c48b686bcfcc67cd598",
+    "url": "/react-calculator/static/js/runtime-main.bee45fa3.js"
+  }
+]);
